f2b0c9522fc1855eb6a77d856f5188f9bba54fb2
Nevidomik <93772215+Nevidomik@users.noreply.github.com>
2022-10-19 22:05:29 +0300

    Add files via upload

---
+++ Lab1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new { Name = "Tom", Age = 34 };
+            Console.WriteLine(user);
+        }
+
+        class animal
+        {
+            public virtual void Move()
+            {
+                Console.WriteLine("Animal move");
+            }
+        }
+
+        class cat : animal
+        {
+            public override void Move()
+            {
+                Console.WriteLine("Cat move");
+            }
+        }
+
+    }
+}
