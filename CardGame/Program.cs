56e9724a9a5be27c1fd8efc641571d0fb62a0833
Nevidomik <93772215+Nevidomik@users.noreply.github.com>
2022-10-19 22:03:31 +0300

    Add files via upload

---
+++ CardGame/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Text;
+
+namespace Cart_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.Reg();
+            game.Play();
+        }
+    }
+}
