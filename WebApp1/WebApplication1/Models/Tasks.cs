f2a93a748e00a53f5442bb8f7df950888d01b91a
Nevidomik <93772215+Nevidomik@users.noreply.github.com>
2022-12-22 21:01:45 +0200

    Add files via upload

---
+++ WebApp1/WebApplication1/Models/Tasks.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Models
+{
+    public class Tasks
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Status { get; set; }
+        public string Priority { get; set; }
+    }
+}
