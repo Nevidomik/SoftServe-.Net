38356cede5842ec62e2c8d73fb525fa95a44e335
Nevidomik <93772215+Nevidomik@users.noreply.github.com>
2022-12-22 23:47:56 +0200

    Add files via upload

---
+++ WebAppBio/WebApplication/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Bio()
+        {
+            return View();
+        }
+        public IActionResult Pol()
+        {
+            return View();
+        }
+    }
+}
